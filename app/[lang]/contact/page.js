01de8a3d3ed063cba8f418b54c9773ccd7ee5c26
CodingSabbir <mdsabbirrahmanm740@gmail.com>
2024-12-07 21:32:37 +0600

    en,bn,fr

---
+++ app/[lang]/contact/page.js
@@ -0,0 +1,10 @@
+import { getDictionary } from "../dictionaries/dictionaries"
+
+export default async function ContactPage({params: {lang}}){
+    const dict = await getDictionary(lang)
+    return(
+        <div>
+            <h1>{dict.contact}</h1>
+        </div>
+    )
+}
